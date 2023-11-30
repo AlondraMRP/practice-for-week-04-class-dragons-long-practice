9d8490ac5793ccc93bc14263a1a709aa27895c26
AlondraMRP <alondra.ramirez.pichardo@gmail.com>
2023-11-30 17:37:54 -0400

    All classes created

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,5 +1,22 @@
-// Your code here
+class Dragon {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
 
+  breathesFire (){
+    return `${this.name} breathes fire everywhere! BURN!!!!`
+  }
+
+  static getDragons(...dragonName){
+    let gatheringOfDragons = [];
+    gatheringOfDragons = dragonName.map((dragon) => dragon.name);
+    return gatheringOfDragons;
+  }
+}
+// const puff = new Dragon("Puff", "green");
+// const toothless = new Dragon("Toothless", "black");
+// console.log(Dragon.getDragons(puff, toothless));
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
--- classes/evil-dragon.js
+++ classes/evil-dragon.js
@@ -1,1 +1,20 @@
-// Your code here
+const Dragon = require("./dragon");
+
+class EvilDragon extends Dragon{
+    constructor(name, color, evilDoings, nemesis) {
+        super(name, color);
+        this.evilDoings = evilDoings;
+        this.nemesis = nemesis;
+    }
+
+    dontInviteThemOverForDinner(){
+        return this.evilDoings.forEach((evils) => console.log(`${this.name} will ${evils}`));
+    }
+
+    burnsNemesis(){
+        return `${this.name} destroys ${this.nemesis} with fire! WHOOOSH!!!`
+    }
+}
+
+
+module.exports = EvilDragon;
